74503e15fb18478f6b2f92be05549aa5954ad05e
kunlong-luo <luokunlong.loong@gmail.com>
2026-09-26 08:23:22 +0800

    test: accept localized starter profile name

--- e2e/product-state-flows.e2e.ts
+++ e2e/product-state-flows.e2e.ts
@@ -74,7 +74,7 @@ test.describe('product state flows', () => {
 
     const profileTrigger = page
       .locator('button:visible')
-      .filter({ hasText: /默认简历|Default/ })
+      .filter({ hasText: /起始简历|Starter Resume|默认简历|Default/ })
       .first();
     await expect(profileTrigger).toBeVisible();
     await profileTrigger.click();
